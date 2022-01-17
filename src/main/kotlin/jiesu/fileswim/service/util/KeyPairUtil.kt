528c1413ecc08d4ce03eb8f1ac48fe684fa9d0f7
Jie Su <jiesu12@gmail.com>
2022-01-17 14:57:47 -0600

    can load key pair using PEM files.

--- src/main/kotlin/jiesu/fileswim/service/util/KeyPairUtil.kt
+++ src/main/kotlin/jiesu/fileswim/service/util/KeyPairUtil.kt
@@ -1,28 +1,72 @@
 package jiesu.fileswim.service.util
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.util.io.pem.PemReader
 import java.io.File
+import java.io.FileReader
 import java.security.KeyFactory
 import java.security.KeyPair
+import java.security.Security
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
 import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
 
 /**
- * Generate a key pair in pem format:
+ * Generate private key in PKCS #1 PEM format:
  * ```
- * openssl genrsa -out token_private_key.pem 3072
+ * openssl genrsa -out tmp.pem 3072
+ * ```
+ * 
+ * Convert PKCS #1 to PKCS #8:
+ * ```
+ * openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in tmp.pem -out token_private_key.pem
+ * ```
+ *
+ * Create public key:
+ * ```
  * openssl rsa -in token_private_key.pem -outform PEM -pubout -out token_public_key.pem
  * ```
  *
- * Then convert the key to DER files for java to read.
+ * Convert PEM format to DER format.
  * ```
  * openssl pkcs8 -topk8 -inform PEM -outform DER -in token_private_key.pem -out token_private_key.der -nocrypt
  * openssl rsa -in token_private_key.pem -pubout -outform DER -out token_public_key.der
  * ```
  */
-fun createKeyPair(privateFile: String, publicFile: String): KeyPair {
+fun loadKeyPairFromDerFile(privateFile: String, publicFile: String): KeyPair {
     val keyFactory = KeyFactory.getInstance("RSA")
     return KeyPair(
         keyFactory.generatePublic(X509EncodedKeySpec(File(publicFile).readBytes())),
         keyFactory.generatePrivate(PKCS8EncodedKeySpec(File(privateFile).readBytes()))
     )
+}
+
+fun loadKeyPairFromPemFile(privateFile: String, publicFile: String): KeyPair =
+    KeyPair(
+        loadPublicKeyFromPemFile(File(publicFile)),
+        loadPrivateKeyFromPemFile(File(privateFile)))
+
+fun loadPublicKeyFromPemFile(file: File): RSAPublicKey {
+    val factory = KeyFactory.getInstance("RSA")
+    FileReader(file).use { keyReader ->
+        PemReader(keyReader).use { pemReader ->
+            val pemObject = pemReader.readPemObject()
+            val content = pemObject.content
+            val pubKeySpec = X509EncodedKeySpec(content)
+            return factory.generatePublic(pubKeySpec) as RSAPublicKey
+        }
+    }
+}
+
+fun loadPrivateKeyFromPemFile(file: File): RSAPrivateKey {
+    val factory = KeyFactory.getInstance("RSA")
+    FileReader(file).use { keyReader ->
+        PemReader(keyReader).use { pemReader ->
+            val pemObject = pemReader.readPemObject()
+            val content = pemObject.content
+            val privateKeySpec = PKCS8EncodedKeySpec(content)
+            return factory.generatePrivate(privateKeySpec) as RSAPrivateKey
+        }
+    }
 }
--- src/test/kotlin/jiesu/fileswim/service/util/KeyPairUtilKtTest.kt
+++ src/test/kotlin/jiesu/fileswim/service/util/KeyPairUtilKtTest.kt
@@ -7,10 +7,18 @@ import org.junit.jupiter.api.Assertions.*
 internal class KeyPairUtilKtTest {
 
     @Test
-    fun createKeyPairTest() {
-        val keyPair = createKeyPair(
+    fun loadKeyPairFromDerFileTest() {
+        val keyPair = loadKeyPairFromDerFile(
             this.javaClass::class.java.getResource("/token_private_key.der").file,
             this.javaClass::class.java.getResource("/token_public_key.der").file)
+        assertNotNull(keyPair)
+    }
+
+    @Test
+    fun loadKeyPairFromPemFileTest() {
+        val keyPair = loadKeyPairFromPemFile(
+            this.javaClass::class.java.getResource("/token_private_key.pem").file,
+            this.javaClass::class.java.getResource("/token_public_key.pem").file)
         assertNotNull(keyPair)
     }
 }
